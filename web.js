738fb4bc5f331f5734ba5daabf4f6ed6769d7a68
Tayyab Wasim <affanwasim@gmail.com>
2013-07-11 15:48:36 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
 var express = require('express');
-var buffer =new Buffer(16);
-buffer.write(''+fs.readFileSync(index.html),'utf-8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World '+buffer.toString('utf-8',12,25))
+    var fs - require('fs');
+    var buffer = new Buffer(fs.readFileSync('index.html'),'utf-8');
+    response.send(buffer.toString());
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
